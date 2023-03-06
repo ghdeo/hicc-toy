9f98b7c01fb45249c51bbf23bc43c880eb683075
ghdeo <goldencorn7@naver.com>
2023-03-06 12:40:04 +0900

    Initial commit

---
+++ src/main/java/hicc/toy/ToyApplication.java
@@ -0,0 +1,13 @@
+package hicc.toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyApplication.class, args);
+	}
+
+}
