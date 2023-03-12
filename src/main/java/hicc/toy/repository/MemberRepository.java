8a6691b8afe60cea9d3dfcf516dc8df131514509
ghdeo <goldencorn7@naver.com>
2023-03-12 16:37:49 +0900

    fix: Change MemberRepository interface to extend JpaRepository

--- src/main/java/hicc/toy/repository/MemberRepository.java
+++ src/main/java/hicc/toy/repository/MemberRepository.java
@@ -1,11 +1,9 @@
 package hicc.toy.repository;
 
 import hicc.toy.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface MemberRepository {
-    void save(Member member);
-
-    Member findById(Long memberId);
-
-    void clear();
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
