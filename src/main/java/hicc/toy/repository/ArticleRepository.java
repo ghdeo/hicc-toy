e7adbbdd715a7f0302b425e2451c68bbd447d771
ghdeo <goldencorn7@naver.com>
2023-04-09 23:55:57 +0900

    feat: Add findAllByDeleteYn to query according to deletion status

--- src/main/java/hicc/toy/repository/ArticleRepository.java
+++ src/main/java/hicc/toy/repository/ArticleRepository.java
@@ -1,9 +1,17 @@
 package hicc.toy.repository;
 
 import hicc.toy.domain.aritcle.Article;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    /*
+     * 게시글 리스트 조회 - (삭제 여부 기준)
+     * */
+    List<Article> findAllByDeleteYn(final char deleteYn, final Sort sort);
 }
