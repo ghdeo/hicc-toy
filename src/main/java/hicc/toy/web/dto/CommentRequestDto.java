75d5532ec9be9aeaa34cb633b92eb6f358adb140
ghdeo <goldencorn7@naver.com>
2023-04-11 01:54:17 +0900

    feat: Add CommentRequestDto

---
+++ src/main/java/hicc/toy/web/dto/CommentRequestDto.java
@@ -0,0 +1,27 @@
+package hicc.toy.web.dto;
+
+import hicc.toy.domain.aritcle.Article;
+import hicc.toy.domain.comment.Comment;
+import hicc.toy.domain.member.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CommentRequestDto {
+    private Long id;
+    private String comment;
+    private Member member;
+    private Article article;
+
+    @Builder
+    public Comment toEntity() {
+        return Comment.builder()
+                .id(id)
+                .comment(comment)
+                .member(member)
+                .article(article)
+                .build();
+    }
+}
