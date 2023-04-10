e0a836712f87bd59fd3ab639c54c6db1df089de4
ghdeo <goldencorn7@naver.com>
2023-04-11 01:52:54 +0900

    feat: Add error status code

--- src/main/java/hicc/toy/exception/ErrorCode.java
+++ src/main/java/hicc/toy/exception/ErrorCode.java
@@ -19,6 +19,16 @@ public enum ErrorCode {
     POSTS_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글 정보를 찾을 수 없습니다."),
 
     /*
+     * 404 NOT_FOUND: 리소스를 찾을 수 없음
+     */
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "멤버 정보를 찾을 수 없습니다."),
+
+    /*
+     * 404 NOT_FOUND: 리소스를 찾을 수 없음
+     */
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글 정보를 찾을 수 없습니다."),
+
+    /*
      * 405 METHOD_NOT_ALLOWED: 허용되지 않은 Request Method 호출
      */
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 메서드입니다."),
