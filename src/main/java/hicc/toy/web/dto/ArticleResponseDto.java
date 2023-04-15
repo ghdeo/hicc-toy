058ca8003874de05e9b2a9364b1638a5fd19dde7
ghdeo <goldencorn7@naver.com>
2023-04-16 01:06:25 +0900

    feat: Add annotations in ArticleResponseDto

--- src/main/java/hicc/toy/web/dto/ArticleResponseDto.java
+++ src/main/java/hicc/toy/web/dto/ArticleResponseDto.java
@@ -3,12 +3,17 @@ package hicc.toy.web.dto;
 import hicc.toy.domain.aritcle.Article;
 import hicc.toy.domain.aritcle.ArticleType;
 import hicc.toy.domain.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class ArticleResponseDto {
     private Long id;
@@ -19,6 +24,7 @@ public class ArticleResponseDto {
     private List<CommentResponseDto> comments;
     private Member member;
 
+    @Builder
     public ArticleResponseDto(Article article) {
         this.id = article.getId();
         this.articleType = article.getArticleType();
