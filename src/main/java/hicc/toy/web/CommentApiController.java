bb3d27abdd034187c900454734a1d3e08c116359
ghdeo <goldencorn7@naver.com>
2023-04-16 01:19:36 +0900

    feat: Add findByArticleId in CommentApiController

--- src/main/java/hicc/toy/web/CommentApiController.java
+++ src/main/java/hicc/toy/web/CommentApiController.java
@@ -2,8 +2,11 @@ package hicc.toy.web;
 
 import hicc.toy.services.CommentService;
 import hicc.toy.web.dto.CommentRequestDto;
+import hicc.toy.web.dto.CommentResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +18,12 @@ public class CommentApiController {
         return commentService.save(requestDto);
     }
 
-    @PutMapping("/article/{articleId}/comments{commentId}")
+    @GetMapping("/article/{id}/comments")
+    public List<CommentResponseDto> findByArticleId(@PathVariable Long id) {
+        return commentService.findByArticleId(id);
+    }
+
+    @PutMapping("/article/{articleId}/comments/{commentId}")
     public Long update(@PathVariable final Long articleId,
                        @PathVariable final Long commentId,
                        @RequestBody CommentRequestDto requestDto) {
