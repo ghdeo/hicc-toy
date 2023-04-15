fe9b5ec4511bfb8438f59987b293471564e16836
ghdeo <goldencorn7@naver.com>
2023-04-16 01:22:45 +0900

    refactor: Modify CommentResponseDto to include Member and Article objects

--- src/main/java/hicc/toy/web/dto/CommentResponseDto.java
+++ src/main/java/hicc/toy/web/dto/CommentResponseDto.java
@@ -1,5 +1,6 @@
 package hicc.toy.web.dto;
 
+import hicc.toy.domain.aritcle.Article;
 import hicc.toy.domain.comment.Comment;
 import hicc.toy.domain.member.Member;
 import lombok.Getter;
@@ -8,13 +9,13 @@ import lombok.Getter;
 public class CommentResponseDto {
     private Long id;
     private String comment;
-    private Long memberId;
-    private Long articleId;
+    private Member member;
+    private Article article;
 
     public CommentResponseDto(Comment comment) {
         this.id = comment.getId();
         this.comment = comment.getComment();
-        this.memberId = comment.getMember().getId();
-        this.articleId = comment.getArticle().getId();
+        this.member = comment.getMember();
+        this.article = comment.getArticle();
     }
 }
