fff60e4f240ff6b62baa4cee4d84038dc1234261
ghdeo <goldencorn7@naver.com>
2023-04-16 01:23:12 +0900

    feat: Add findByArticleId in CommentService

--- src/main/java/hicc/toy/services/CommentService.java
+++ src/main/java/hicc/toy/services/CommentService.java
@@ -7,9 +7,13 @@ import hicc.toy.repository.ArticleRepository;
 import hicc.toy.repository.CommentRepository;
 import hicc.toy.repository.MemberRepository;
 import hicc.toy.web.dto.CommentRequestDto;
+import hicc.toy.web.dto.CommentResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -19,6 +23,15 @@ public class CommentService {
     @Transactional
     public Long save(CommentRequestDto requestDto) {
         return commentRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<CommentResponseDto> findByArticleId(Long articleId) {
+        List<Comment> comments = commentRepository.findByArticleId(articleId).orElseThrow(() -> new CustomException(ErrorCode.COMMENT_NOT_FOUND));
+        return comments.stream()
+                .map(CommentResponseDto::new)
+                .collect(Collectors.toList());
+
     }
 
     @Transactional
